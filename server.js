8ad40544de3650228ff93f1bb752241fee0072ab
kazweda <kazuaki.ueda@gmail.com>
2022-11-28 02:53:54 +0000

    #1 Expressに更新

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
+// const port = 3000;
+// import { createServer } from "http";
+
+// const server = createServer((request, response) => {
+//     response.writeHead(200, {
+//       "Content-Type": "text/html"
+//     });
+
+//     const responseMessage = "<h1>Hello Node.js!</h1>";
+//     response.write(responseMessage);
+//     response.end();
+//     console.log(`Sent a response : ${responseMessage}`);
+// });
+
+// server.listen(port);
+// console.log(`The server has started and is listening on port number: ${port}`);
+
 const port = 3000;
-import { createServer } from "http";
+import express from "express";
+const app = express();
 
-const server = createServer((request, response) => {
-    response.writeHead(200, {
-      "Content-Type": "text/html"
-    });
-
-    const responseMessage = "<h1>Hello Node.js!</h1>";
-    response.write(responseMessage);
-    response.end();
-    console.log(`Sent a response : ${responseMessage}`);
+app.get("/", (request, response) => {
+    response.send("<h1>Hello Express.js!</h1>");
+}).listen(port, () => {
+    console.log(`The server has started and is listening on port number: ${port}`);
 });
-
-server.listen(port);
-console.log(`The server has started and is listening on port number: ${port}`);
