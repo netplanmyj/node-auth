64a67078d8b295ead1a62eb2bd596811daafcc46
kazweda <kazuaki.ueda@gmail.com>
2022-12-20 08:18:28 +0000

    fix #11

--- todo-app/routes/login.js
+++ todo-app/routes/login.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const knex = require('../db/knex');
-const bcrypt = require("bcrypt");
 const router = express.Router();
+const passport = require("passport");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
@@ -12,44 +11,11 @@ router.get('/', function (req, res, next) {
   });
 });
 
-router.post('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
-  const username = req.body.username;
-  const password = req.body.password;
-
-  knex("users")
-    .where({
-      name: username,
-    })
-    .select("*")
-    .then(async function (results) {
-      const comparedPassword = await bcrypt.compare(password, results[0].password);
-      if (results.length === 0) {
-        res.render("login", {
-          title: "Login",
-          isAuth: isAuth,
-          errorMessage: ["ユーザが見つかりません"],
-        });
-      } else if (await bcrypt.compare(password, results[0].password)) {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      } else {
-        res.render("login", {
-          title: "Login",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("login", {
-        title: "Login",
-        isAuth: isAuth,
-        errorMessage: [err.sqlMessage],
-      });
-    });
-});
+router.post('/', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}
+));
 
 module.exports = router;
